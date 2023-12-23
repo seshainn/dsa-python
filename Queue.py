9e5d1935ae94000f43a8b8048d56034baca4bcf7
seshu <seshu.giri@gmail.com>
2023-12-23 17:04:42 +0530

    Add: Stack and Queue

---
+++ Queue.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+        
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
+        else:
+            self.last.next = new_node
+            self.last = new_node
+        self.length += 1
+        return True
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        else:
+            temp=self.first
+            self.first=temp.next
+            self.length -= 1
+            return temp
